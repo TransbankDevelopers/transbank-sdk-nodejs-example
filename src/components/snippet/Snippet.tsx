85b3e68bfacf8a5d022e3b999c565722f4ab5f8d
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-06 09:15:19 -0400

    feat: snippet component

---
+++ src/components/snippet/Snippet.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import { CopyBlock, dracula } from "react-code-blocks";
+
+export enum SnippetLanguage {
+  PYTHON = "python",
+  JAVASCRIPT = "javascript",
+}
+
+export type SnippetProps = {
+  code: string;
+  language?: SnippetLanguage;
+  showLineNumbers?: boolean;
+  removeTopPadding?: boolean;
+};
+
+export const Snippet = (props: SnippetProps) => {
+  return (
+    <CopyBlock
+      text={props.code}
+      language={props.language || SnippetLanguage.JAVASCRIPT}
+      showLineNumbers={props.showLineNumbers || false}
+      codeBlock
+      theme={dracula}
+      customStyle={{
+        padding: "12px",
+        borderRadius: "0px",
+        ...(props.removeTopPadding && { paddingTop: "0px" }),
+      }}
+    />
+  );
+};
