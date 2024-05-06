aa237c8c6a34f2b8c8b9f2e68fd7df8592a6dbae
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-06 09:08:49 -0400

    feat: consts file for the project

---
+++ src/consts.ts
@@ -0,0 +1,50 @@
+import { Route } from "./types/menu";
+
+export const webpayPlusRoutes: Route[] = [
+  {
+    name: "Webpay Plus",
+    path: "/webpay-plus",
+  },
+  {
+    name: "Webpay Plus Diferido",
+    path: "/webpay-plus-deferred",
+  },
+  {
+    name: "Webpay Mall",
+    path: "/webpay-mall",
+  },
+  {
+    name: "Webpay Mall Diferido",
+    path: "/webpay-mall-deferred",
+  },
+];
+
+export const webpayOneClickRoutes: Route[] = [
+  {
+    name: "Oneclick Mall",
+    path: "/oneclick-mall",
+  },
+  {
+    name: "Oneclick Mall Diferido",
+    path: "/oneclick-mall-deferred",
+  },
+];
+
+export const webpayFullTransactionRoutes: Route[] = [
+  {
+    name: "Transacción Completa",
+    path: "/full-transaction",
+  },
+  {
+    name: "Transacción Completa Diferido",
+    path: "/full-transaction-deferred",
+  },
+  {
+    name: "Transacción Completa Mall",
+    path: "/full-transaction-mall",
+  },
+  {
+    name: "Transacción Completa Mall Diferido",
+    path: "/full-transaction-mall-deferred",
+  },
+];
