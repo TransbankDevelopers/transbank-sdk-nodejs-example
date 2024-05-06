60b189436b9236663e93f6096dac748f988a88fe
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-06 09:15:26 -0400

    feat: step component

---
+++ src/components/step/Step.tsx
@@ -0,0 +1,22 @@
+import { Snippet } from "../snippet/Snippet";
+import { Text, TextType } from "../text/Text";
+
+export type StepProps = {
+  code?: string;
+  stepTitle: string;
+  content: React.ReactNode;
+};
+
+export const Step = (props: StepProps) => {
+  return (
+    <div className="flex flex-col">
+      <Text type={TextType.SECTION_TITLE}>{props.stepTitle}</Text>
+      <Text>{props.content}</Text>
+      {props.code && (
+        <div className="my-4">
+          <Snippet code={props.code} removeTopPadding />
+        </div>
+      )}
+    </div>
+  );
+};
