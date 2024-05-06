814cbe0544a8a0c668fcd23716e101556eac4c5c
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-06 10:14:42 -0400

    feat: new types for commit response

--- src/types/transactions.tsx
+++ src/types/transactions.tsx
@@ -9,3 +9,29 @@ export type TBKCreateTransactionResponse = {
   token: string;
   url: string;
 };
+
+export enum TBKTransactionStatus {
+  AUTHORIZED = "AUTHORIZED",
+}
+
+export type TBKCardDetail = {
+  card_number: string;
+};
+
+export enum PaymentTypeCode {
+  VN = "VN",
+}
+
+export type TBKCommitTransactionResponse = {
+  amount: number;
+  status: TBKTransactionStatus.AUTHORIZED;
+  buy_order: string;
+  session_id: string;
+  card_detail: TBKCardDetail;
+  accounting_date: number;
+  transaction_date: string;
+  authorization_code: string;
+  payment_type_code: PaymentTypeCode;
+  response_code: number;
+  installments_number: number;
+};
