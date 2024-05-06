98332bf96c7cff1f1f119e4174003688d732a6bf
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-06 18:46:03 -0400

    refactor: use new layout component on views

--- src/pages/webpay-plus/commit.tsx
+++ src/pages/webpay-plus/commit.tsx
@@ -4,7 +4,7 @@ import { Menu } from "@/components/layout/Menu";
 import { Text, TextType } from "@/components/text/Text";
 import { Breadcrumbs } from "@/components/layout/Breadcrumbs";
 import { Route } from "@/types/menu";
-import { Step } from "@/components/step/Step";
+import { Step, StepProps } from "@/components/step/Step";
 import { HelpMenu } from "@/components/layout/HelpMenu";
 import * as commitSnippets from "@/helpers/webpay-plus/snippets/commit";
 import { Table } from "@/components/table/Table";
@@ -23,6 +23,7 @@ import { Card } from "@/components/card/Card";
 import { Questions } from "@/components/questions/Questions";
 import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { WebpayPlus } from "transbank-sdk";
+import { Layout } from "@/components/layout/Layout";
 
 const actualBread: Route[] = [
   {
@@ -40,116 +41,98 @@ export type CommitTransactionProps = {
   commitResponse: TBKCommitTransactionResponse;
 };
 
-export default function CommitTransaction(props: CommitTransactionProps) {
-  return (
-    <div className="flex flex-col">
-      <Header />
-      <div className="grid grid-cols-[280px,1fr,248px] px-24 py-10">
-        <Menu />
-        <div className="pl-20 pr-12 mt-10 flex flex-col overflow-auto">
-          <div className="mb-6">
-            <Breadcrumbs items={actualBread} active="/webpay-plus" />
-          </div>
-          <div className="mb-8 flex flex-col">
-            <Text type={TextType.PAGE_TITLE}>
-              Webpay Plus - Confirmar transacción
-            </Text>
+const getSteps = (
+  token: string,
+  commitResponse: TBKCommitTransactionResponse
+): StepProps[] => {
+  return [
+    {
+      stepTitle: "Paso 1: Datos recibidos",
+      content: `Después de completar el flujo en el formulario de pago, 
+recibirás un POST con la siguiente información:`,
+      code: commitSnippets.getStepOne(token),
+    },
+    {
+      stepTitle: "Paso 2: Petición",
+      content: `Utilizarás el token recibido para confirmar la transacción 
+mediante el SDK.`,
+      code: commitSnippets.getStepTwo(),
+    },
+    {
+      stepTitle: "Paso 3: Respuesta",
+      content: `Transbank responderá con la siguiente información. Es crucial guardar
+esta respuesta, y la única validación necesaria es que el campo &quot;response_code&quot; 
+sea igual a cero.`,
+      code: commitSnippets.getStepThree(commitResponse),
+    },
+    {
+      stepTitle: "¡Listo!",
+      content: (
+        <div className="flex flex-col gap-5">
+          <Text>
+            Con la confirmación exitosa, ya puedes mostrar al usuario una página
+            de éxito de la transacción, proporcionándole la tranquilidad de que
+            el proceso ha sido completado con éxito.
+          </Text>
+          <div className="flex flex-col gap-2">
             <Text>
-              En este paso es importante confirmar la transacción para notificar
-              a Transbank que hemos recibido exitosamente los detalles de la
-              transacción.{" "}
-              <b>
-                Es importante destacar que si la confirmación no se realiza, la
-                transacción será reversada.
-              </b>
+              Después de confirmar la transacción, podrás realizar otras
+              operaciones útiles:
             </Text>
-          </div>
-          <div className="flex flex-col gap-y-4">
-            <Step
-              stepTitle="Paso 1: Datos recibidos"
-              content={
-                <Text>
-                  Después de completar el flujo en el formulario de pago,
-                  recibirás un POST con la siguiente información:
-                </Text>
-              }
-              code={commitSnippets.getStepOne(props.token)}
-            />
-            <Step
-              stepTitle="Paso 2: Petición"
-              content={
-                <Text>
-                  Utilizarás el token recibido para confirmar la transacción
-                  mediante el SDK.
-                </Text>
-              }
-              code={commitSnippets.getStepTwo()}
-            />
-            <Step
-              stepTitle="Paso 3: Respuesta"
-              content={
-                <Text>
-                  Transbank responderá con la siguiente información. Es crucial
-                  guardar esta respuesta, y la única validación necesaria es que
-                  el campo &quot;response_code&quot; sea igual a cero.
-                </Text>
-              }
-              code={commitSnippets.getStepThree(props.commitResponse)}
-            />
-            <Step
-              stepTitle="¡Listo!"
-              content={
-                <div className="flex flex-col gap-5">
-                  <Text>
-                    Con la confirmación exitosa, ya puedes mostrar al usuario
-                    una página de éxito de la transacción, proporcionándole la
-                    tranquilidad de que el proceso ha sido completado con éxito.
-                  </Text>
-                  <div className="flex flex-col gap-2">
-                    <Text>
-                      Después de confirmar la transacción, podrás realizar otras
-                      operaciones útiles:
-                    </Text>
-                    <ul className="list-disc px-6">
-                      <li>
-                        <b>Reembolsar:</b> Puedes reversar o anular el pago
-                        según ciertas condiciones comerciales.
-                      </li>
-                      <li>
-                        <b>Consultar Estado:</b> Hasta 7 días después de
-                        realizada la transacción, podrás consultar el estado de
-                        la transacción.
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              }
-            />
-            <Card>
-              <span className="font-medium text-sm mb-8">
-                Formulario de redirección
-              </span>
-              <InputText value={"xd"} onChange={() => {}} />
-              <div className="flex justify-end mt-6">
-                <form action={"rr"} method="POST">
-                  <input type="hidden" name="token_ws" value={"xd"} />
-                  <Button
-                    text="PAGAR"
-                    className="max-w-[94px]"
-                    type={ButtonTypes.SUBMIT}
-                  />
-                </form>
-              </div>
-            </Card>
-          </div>
-          <div className="my-12">
-            <Questions />
+            <ul className="list-disc px-6">
+              <li>
+                <b>Reembolsar:</b> Puedes reversar o anular el pago según
+                ciertas condiciones comerciales.
+              </li>
+              <li>
+                <b>Consultar Estado:</b> Hasta 7 días después de realizada la
+                transacción, podrás consultar el estado de la transacción.
+              </li>
+            </ul>
           </div>
         </div>
-        <HelpMenu />
-      </div>
-      <div className="bg-tbk-black-bg h-[200px]"></div>
-    </div>
+      ),
+    },
+  ];
+};
+
+export default function CommitTransaction(props: CommitTransactionProps) {
+  return (
+    <Layout
+      pageTitle="Webpay Plus - Confirmar transacción"
+      pageDescription={
+        <>
+          En este paso es importante confirmar la transacción para notificar a
+          Transbank que hemos recibido exitosamente los detalles de la
+          transacción.{" "}
+          <b>
+            Es importante destacar que si la confirmación no se realiza, la
+            transacción será reversada.
+          </b>
+        </>
+      }
+      actualBread={actualBread}
+      activeRoute="/webpay-plus"
+      steps={getSteps(props.token, props.commitResponse)}
+      additionalContent={
+        <Card>
+          <span className="font-medium text-sm mb-8">
+            Formulario de redirección
+          </span>
+          <InputText value={"xd"} onChange={() => {}} />
+          <div className="flex justify-end mt-6">
+            <form action={"rr"} method="POST">
+              <input type="hidden" name="token_ws" value={"xd"} />
+              <Button
+                text="PAGAR"
+                className="max-w-[94px]"
+                type={ButtonTypes.SUBMIT}
+              />
+            </form>
+          </div>
+        </Card>
+      }
+    />
   );
 }
 
--- src/pages/webpay-plus/index.tsx
+++ src/pages/webpay-plus/index.tsx
@@ -1,29 +1,20 @@
 import "@/app/globals.css";
-import { Header } from "@/components/layout/Header";
-import { Menu } from "@/components/layout/Menu";
-import { Text, TextType } from "@/components/text/Text";
-import { Breadcrumbs } from "@/components/layout/Breadcrumbs";
 import { Route } from "@/types/menu";
-import { Step } from "@/components/step/Step";
-import { HelpMenu } from "@/components/layout/HelpMenu";
-import * as createSnippets from "@/helpers/webpay-plus/snippets/create";
-import { ColumnDefinition, Table } from "@/components/table/Table";
+import { StepProps } from "@/components/step/Step";
+import { Table } from "@/components/table/Table";
 import { InputText } from "@/components/input/InputText";
 import {
   StartTransactionData,
   TBKCreateTransactionResponse,
 } from "@/types/transactions";
-import {
-  generateRandomTransactionData,
-  getColumnDefinition,
-  getColumnValues,
-} from "@/helpers/webpay-plus/transactionHelper";
+import { generateRandomTransactionData } from "@/helpers/webpay-plus/transactionHelper";
 import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { WebpayPlus } from "transbank-sdk";
 import { useState } from "react";
 import { Button, ButtonTypes } from "@/components/button/Button";
 import { Card } from "@/components/card/Card";
-import { Questions } from "@/components/questions/Questions";
+import { Layout } from "@/components/layout/Layout";
+import { getCreateTRXSteps } from "@/helpers/webpay-plus/createSteps";
 
 const actualBread: Route[] = [
   {
@@ -39,7 +30,7 @@ const actualBread: Route[] = [
 export type CreateTRXProps = TBKCreateTransactionResponse &
   StartTransactionData;
 
-export default function Home(props: CreateTRXProps) {
+export default function CreateTRX(props: CreateTRXProps) {
   const [tokenInput, setTokenInput] = useState(props.token);
 
   const handleTokenInputChange = (value: string) => {
@@ -47,114 +38,34 @@ export default function Home(props: CreateTRXProps) {
   };
 
   return (
-    <div className="flex flex-col">
-      <Header />
-      <div className="grid grid-cols-[280px,1fr,248px] px-24 py-10">
-        <Menu />
-        <div className="pl-20 pr-12 mt-10 flex flex-col overflow-auto">
-          <div className="mb-6">
-            <Breadcrumbs items={actualBread} active="/webpay-plus" />
+    <Layout
+      pageTitle="Webpay Plus - Creación de transacción"
+      pageDescription="En esta etapa, se procederá a la creación de una transacción con
+    el fin de obtener un identificador único. Esto nos permitirá
+    redirigir al Tarjetahabiente hacia el formulario de pago de
+    Transbank en el siguiente paso."
+      actualBread={actualBread}
+      activeRoute="/webpay-plus"
+      steps={getCreateTRXSteps(props.token, props)}
+      additionalContent={
+        <Card>
+          <span className="font-medium text-sm mb-8">
+            Formulario de redirección
+          </span>
+          <InputText value={tokenInput} onChange={handleTokenInputChange} />
+          <div className="flex justify-end mt-6">
+            <form action={props.url} method="POST">
+              <input type="hidden" name="token_ws" value={tokenInput} />
+              <Button
+                text="PAGAR"
+                className="max-w-[94px]"
+                type={ButtonTypes.SUBMIT}
+              />
+            </form>
           </div>
-          <div className="mb-8 flex flex-col">
-            <Text type={TextType.PAGE_TITLE}>
-              Webpay Plus - Creación de transacción
-            </Text>
-            <Text>
-              En esta etapa, se procederá a la creación de una transacción con
-              el fin de obtener un identificador único. Esto nos permitirá
-              redirigir al Tarjetahabiente hacia el formulario de pago de
-              Transbank en el siguiente paso.
-            </Text>
-          </div>
-          <div className="flex flex-col gap-y-4">
-            <Step
-              stepTitle="Paso 1: Petición"
-              content={
-                <div className="flex flex-col gap-y-3">
-                  <span>
-                    1. Comienza por importar la librería WebpayPlus en tu
-                    proyecto.
-                  </span>
-                  <span>
-                    2. Luego, crea una transacción utilizando las funciones
-                    proporcionadas mediante el SDK.
-                  </span>
-                </div>
-              }
-              code={createSnippets.getStepOne()}
-            />
-            <Step
-              stepTitle="Paso 2: Respuesta"
-              content={
-                <span>
-                  Una vez que hayas creado la transacción, aquí encontrarás los
-                  datos de respuesta generados por el proceso.
-                </span>
-              }
-              code={createSnippets.getStepTwo(props.token)}
-            />
-            <Step
-              stepTitle="Paso 3: Creación del formulario"
-              content={
-                <span>
-                  Utiliza estos datos de respuesta para redireccionar al usuario
-                  al formulario de pago al Tarjetahabiente. Este formulario será
-                  la interfaz a través de la cual el usuario realizará su
-                  transacción.
-                </span>
-              }
-              code={createSnippets.getStepThree(props.token)}
-            />
-            <Step
-              stepTitle="Ejemplo"
-              content={
-                <div className="flex flex-col gap-y-5">
-                  <span>
-                    Para llevar a cabo una transacción de compra en nuestro
-                    sistema, primero debemos crear la transacción. Utilizaremos
-                    los siguientes datos para configurar la transacción:
-                  </span>
-                  <div className="my-2">
-                    <Table
-                      columns={getColumnDefinition()}
-                      rows={getColumnValues(props)}
-                    />
-                  </div>
-                  <span>
-                    Por último, con la respuesta del servicio que confirma la
-                    creación de la transacción, procedemos a crear el formulario
-                    de pago. Para fines de este ejemplo, haremos visible el
-                    campo &quot;token_ws&quot;, el cual es esencial para
-                    completar el proceso de pago de manera exitosa.
-                  </span>
-                </div>
-              }
-            />
-            <Card>
-              <span className="font-medium text-sm mb-8">
-                Formulario de redirección
-              </span>
-              <InputText value={tokenInput} onChange={handleTokenInputChange} />
-              <div className="flex justify-end mt-6">
-                <form action={props.url} method="POST">
-                  <input type="hidden" name="token_ws" value={tokenInput} />
-                  <Button
-                    text="PAGAR"
-                    className="max-w-[94px]"
-                    type={ButtonTypes.SUBMIT}
-                  />
-                </form>
-              </div>
-            </Card>
-          </div>
-          <div className="my-12">
-            <Questions />
-          </div>
-        </div>
-        <HelpMenu />
-      </div>
-      <div className="bg-tbk-black-bg h-[200px]"></div>
-    </div>
+        </Card>
+      }
+    />
   );
 }
 
