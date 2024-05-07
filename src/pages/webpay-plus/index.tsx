5f74b6372f8d8c8854dd335186261513c4c9f567
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-06 23:02:30 -0400

    refactor: update create trx view

--- src/pages/webpay-plus/index.tsx
+++ src/pages/webpay-plus/index.tsx
@@ -1,7 +1,4 @@
-import "@/app/globals.css";
 import { Route } from "@/types/menu";
-import { StepProps } from "@/components/step/Step";
-import { Table } from "@/components/table/Table";
 import { InputText } from "@/components/input/InputText";
 import {
   StartTransactionData,
@@ -14,7 +11,8 @@ import { useState } from "react";
 import { Button, ButtonTypes } from "@/components/button/Button";
 import { Card } from "@/components/card/Card";
 import { Layout } from "@/components/layout/Layout";
-import { getCreateTRXSteps } from "@/helpers/webpay-plus/createSteps";
+import { getCreateTRXSteps } from "@/helpers/webpay-plus/steps/createSteps";
+import Head from "next/head";
 
 const actualBread: Route[] = [
   {
@@ -30,7 +28,7 @@ const actualBread: Route[] = [
 export type CreateTRXProps = TBKCreateTransactionResponse &
   StartTransactionData;
 
-export default function CreateTRX(props: CreateTRXProps) {
+export default function CreateTransaction(props: CreateTRXProps) {
   const [tokenInput, setTokenInput] = useState(props.token);
 
   const handleTokenInputChange = (value: string) => {
@@ -38,34 +36,43 @@ export default function CreateTRX(props: CreateTRXProps) {
   };
 
   return (
-    <Layout
-      pageTitle="Webpay Plus - Creación de transacción"
-      pageDescription="En esta etapa, se procederá a la creación de una transacción con
+    <>
+      <Head>
+        <title>Transbank SDK Node - Create Transaction</title>
+      </Head>
+      <Layout
+        pageTitle="Webpay Plus - Creación de transacción"
+        pageDescription="En esta etapa, se procederá a la creación de una transacción con
     el fin de obtener un identificador único. Esto nos permitirá
     redirigir al Tarjetahabiente hacia el formulario de pago de
     Transbank en el siguiente paso."
-      actualBread={actualBread}
-      activeRoute="/webpay-plus"
-      steps={getCreateTRXSteps(props.token, props)}
-      additionalContent={
-        <Card>
-          <span className="font-medium text-sm mb-8">
-            Formulario de redirección
-          </span>
-          <InputText value={tokenInput} onChange={handleTokenInputChange} />
-          <div className="flex justify-end mt-6">
-            <form action={props.url} method="POST">
-              <input type="hidden" name="token_ws" value={tokenInput} />
-              <Button
-                text="PAGAR"
-                className="max-w-[94px]"
-                type={ButtonTypes.SUBMIT}
-              />
-            </form>
-          </div>
-        </Card>
-      }
-    />
+        actualBread={actualBread}
+        activeRoute="/webpay-plus"
+        steps={getCreateTRXSteps(props.token, props)}
+        additionalContent={
+          <Card className="flex-col">
+            <span className="font-medium text-sm mb-8">
+              Formulario de redirección
+            </span>
+            <InputText
+              label="Token"
+              value={tokenInput}
+              onChange={handleTokenInputChange}
+            />
+            <div className="flex justify-end mt-6">
+              <form action={props.url} method="POST">
+                <input type="hidden" name="token_ws" value={tokenInput} />
+                <Button
+                  text="PAGAR"
+                  className="max-w-[94px]"
+                  type={ButtonTypes.SUBMIT}
+                />
+              </form>
+            </div>
+          </Card>
+        }
+      />
+    </>
   );
 }
 
