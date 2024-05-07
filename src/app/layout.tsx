e2ee9d88ef7598e30d9784d8e8286fd37a5769b3
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-06 22:57:22 -0400

    refactor: use roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -10,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
