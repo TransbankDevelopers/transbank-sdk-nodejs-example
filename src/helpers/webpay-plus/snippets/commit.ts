4f97562fe256c7e53e10dab5c9a872e8ace9c600
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-07 09:27:16 -0400

    style: adjust snippets spaces to fit content

--- src/helpers/webpay-plus/snippets/commit.ts
+++ src/helpers/webpay-plus/snippets/commit.ts
@@ -1,17 +1,15 @@
 import { TBKCommitTransactionResponse } from "@/types/transactions";
 
 export const getStepOne = (token: string) => {
-  return `
-  {
-    'token_ws': ${token}
-  }`;
+  return `{
+  'token_ws': ${token}
+}`;
 };
 
 export const getStepTwo = () => {
-  return `
-  // En el caso de Express
-  let token = request.body.token_ws;
-  const commitResponse = await (new WebpayPlus.Transaction()).commit(token);`;
+  return `// En el caso de Express
+let token = request.body.token_ws;
+const commitResponse = await (new WebpayPlus.Transaction()).commit(token);`;
 };
 
 export const getStepThree = (commitResponse: TBKCommitTransactionResponse) => {
@@ -28,20 +26,19 @@ export const getStepThree = (commitResponse: TBKCommitTransactionResponse) => {
     response_code,
     installments_number,
   } = commitResponse;
-  return `
-  {
-    "amount": ${amount},
-    "status": "${status}",
-    "buy_order": "${buy_order}",
-    "session_id": "${session_id}",
-    "card_detail": {
-     "card_number": "${card_detail.card_number}",
-    },
-    "accounting_date": "${accounting_date}",
-    "transaction_date": "${transaction_date}",
-    "authorization_code": "${authorization_code}",
-    "payment_type_code": "${payment_type_code}",
-    "response_code": "${response_code}",
-    "installments_number": "${installments_number}"
-   }`;
+  return `{
+  "amount": ${amount},
+  "status": "${status}",
+  "buy_order": "${buy_order}",
+  "session_id": "${session_id}",
+  "card_detail": {
+    "card_number": "${card_detail.card_number}",
+  },
+  "accounting_date": "${accounting_date}",
+  "transaction_date": "${transaction_date}",
+  "authorization_code": "${authorization_code}",
+  "payment_type_code": "${payment_type_code}",
+  "response_code": "${response_code}",
+  "installments_number": "${installments_number}"
+  }`;
 };
