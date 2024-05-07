eaad6183155c9e4a3a8221c7065b85182c21ea53
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-07 09:28:04 -0400

    refactor: rename steps file name

--- src/helpers/webpay-plus/steps/commit.tsx
+++ src/helpers/webpay-plus/steps/commit.tsx
@@ -10,40 +10,50 @@ export const getCommitSteps = (
   return [
     {
       stepTitle: "Paso 1: Datos recibidos",
-      content: `Después de completar el flujo en el formulario de pago, 
-  recibirás un POST con la siguiente información:`,
+      content: (
+        <Text>
+          Después de completar el flujo en el formulario de pago, recibirás un
+          POST con la siguiente información:
+        </Text>
+      ),
       code: commitSnippets.getStepOne(token),
     },
     {
       stepTitle: "Paso 2: Petición",
-      content: `Utilizarás el token recibido para confirmar la transacción 
-  mediante el SDK.`,
+      content: (
+        <Text>
+          Utilizarás el token recibido para confirmar la transacción mediante el
+          SDK.
+        </Text>
+      ),
       code: commitSnippets.getStepTwo(),
     },
     {
       stepTitle: "Paso 3: Respuesta",
-      content: `Transbank responderá con la siguiente información. Es crucial guardar
-  esta respuesta, y la única validación necesaria es que el campo "response_code" 
-  sea igual a cero.`,
+      content: (
+        <Text>
+          Transbank responderá con la siguiente información. Es crucial guardar
+          esta respuesta, y la única validación necesaria es que el campo
+          &quot;response_code&quot; sea igual a cero.
+        </Text>
+      ),
       code: commitSnippets.getStepThree(commitResponse),
     },
     {
       stepTitle: "¡Listo!",
       content: (
-        <>
-          <div className="mb-4">
-            <Text>
-              Con la confirmación exitosa, ya puedes mostrar al usuario una
-              página de éxito de la transacción, proporcionándole la
-              tranquilidad de que el proceso ha sido completado con éxito.
-            </Text>
-          </div>
+        <div className="flex flex-col gap-4">
+          <Text>
+            Con la confirmación exitosa, ya puedes mostrar al usuario una página
+            de éxito de la transacción, proporcionándole la tranquilidad de que
+            el proceso ha sido completado con éxito.
+          </Text>
           <div className="flex flex-col gap-2">
             <Text>
               Después de confirmar la transacción, podrás realizar otras
               operaciones útiles:
             </Text>
-            <ul className="list-disc px-6 mt-2">
+            <ul className="list-disc px-6 text-base text-tbk-black-2">
               <li>
                 <b>Reembolsar:</b> Puedes reversar o anular el pago según
                 ciertas condiciones comerciales.
@@ -54,7 +64,7 @@ export const getCommitSteps = (
               </li>
             </ul>
           </div>
-        </>
+        </div>
       ),
     },
   ];
--- src/helpers/webpay-plus/steps/create.tsx
+++ src/helpers/webpay-plus/steps/create.tsx
@@ -3,6 +3,7 @@ import { StartTransactionData } from "@/types/transactions";
 import * as createSnippets from "@/helpers/webpay-plus/snippets/create";
 import { Table } from "@/components/table/Table";
 import { getColumnDefinition, getColumnValues } from "../transactionHelper";
+import { Text } from "@/components/text/Text";
 
 export const getCreateTRXSteps = (
   token: string,
@@ -13,13 +14,13 @@ export const getCreateTRXSteps = (
       stepTitle: "Paso 1: Petición",
       content: (
         <div className="flex flex-col gap-y-3">
-          <span>
+          <Text>
             1. Comienza por importar la librería WebpayPlus en tu proyecto.
-          </span>
-          <span>
+          </Text>
+          <Text>
             2. Luego, crea una transacción utilizando las funciones
             proporcionadas mediante el SDK.
-          </span>
+          </Text>
         </div>
       ),
       code: createSnippets.getStepOne(),
@@ -27,44 +28,46 @@ export const getCreateTRXSteps = (
     {
       stepTitle: "Paso 2: Respuesta",
       content: (
-        <span>
+        <Text>
           Una vez que hayas creado la transacción, aquí encontrarás los datos de
           respuesta generados por el proceso.
-        </span>
+        </Text>
       ),
       code: createSnippets.getStepTwo(token),
     },
     {
       stepTitle: "Paso 3: Creación del formulario",
       content: (
-        <span>
+        <Text>
           Utiliza estos datos de respuesta para redireccionar al usuario al
           formulario de pago al Tarjetahabiente. Este formulario será la
           interfaz a través de la cual el usuario realizará su transacción.
-        </span>
+        </Text>
       ),
       code: createSnippets.getStepThree(token),
     },
     {
       stepTitle: "Ejemplo",
       content: (
-        <div className="flex flex-col gap-12">
-          <span>
+        <div className="flex flex-col gap-8">
+          <Text>
             Para llevar a cabo una transacción de compra en nuestro sistema,
             primero debemos crear la transacción. Utilizaremos los siguientes
             datos para configurar la transacción:
-          </span>
+          </Text>
+
           <Table
             columns={getColumnDefinition()}
             rows={getColumnValues(startTransactionData)}
           />
-          <span>
+
+          <Text>
             Por último, con la respuesta del servicio que confirma la creación
             de la transacción, procedemos a crear el formulario de pago. Para
             fines de este ejemplo, haremos visible el campo
             &quot;token_ws&quot;, el cual es esencial para completar el proceso
             de pago de manera exitosa.
-          </span>
+          </Text>
         </div>
       ),
     },
