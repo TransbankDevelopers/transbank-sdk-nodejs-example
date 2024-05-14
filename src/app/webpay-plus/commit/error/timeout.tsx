ba16b31356ff692aa30e15c5a779de5bf17d43b0
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-14 08:54:57 -0400

    feat: add error handling for aborted transactions in webpay plus

---
+++ src/app/webpay-plus/commit/error/timeout.tsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import { Route } from "@/types/menu";
+import { Layout } from "@/components/layout/Layout";
+import { getErrorTimeoutSteps } from "../../content/steps/error-timeout";
+import { TBKTimeoutResponse } from "@/types/transactions";
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Plus",
+    path: "/webpay-plus",
+  },
+  {
+    name: "Time out",
+    path: "/webpay-plus/commit",
+  },
+];
+
+export type TimeoutViewProps = {
+  timeoutResponse: TBKTimeoutResponse;
+  token_ws: string;
+};
+
+export const TimeoutView = async (props: TimeoutViewProps) => {
+  return (
+    <>
+      <Head>
+        <title>Transbank SDK Node - Time out</title>
+      </Head>
+      <Layout
+        pageTitle="Webpay Plus - Time out"
+        pageDescription={
+          <span>
+            Cuando una transacción expira debido a un timeout, es crucial
+            gestionar este escenario de manera adecuada para garantizar la
+            transparencia y la experiencia del usuario, para la prueba en
+            integración son de 10 minutos.
+          </span>
+        }
+        actualBread={actualBread}
+        activeRoute="/webpay-plus/commit"
+        steps={getErrorTimeoutSteps(props.timeoutResponse)}
+      />
+    </>
+  );
+};
--- src/app/webpay-plus/commit/page.tsx
+++ src/app/webpay-plus/commit/page.tsx
@@ -4,29 +4,86 @@ import { Layout } from "@/components/layout/Layout";
 import { getCommitSteps } from "@/app/webpay-plus/content/steps/commit";
 import Head from "next/head";
 import { commitTransaction } from "@/app/lib/webpay-plus/data";
-import { NextPageProps } from "@/types/general";
+import { NextPageProps, SearchParams } from "@/types/general";
 import { RefundAndStatus } from "./components/RefundAndStatus";
+import { AbortedView } from "./error/aborted";
+import {
+  TBKAbortedResponse,
+  TBKCallbackType,
+  TBKCommitTransactionResponse,
+  TBKTimeoutResponse,
+  TBKTransactionStatus,
+} from "@/types/transactions";
+import { TimeoutView } from "./error/timeout";
 
-const actualBread: Route[] = [
-  {
-    name: "Inicio",
-    path: "/",
-  },
-  {
-    name: "Webpay Plus",
-    path: "/webpay-plus",
-  },
-  {
-    name: "Confirmar transacción",
-    path: "/webpay-plus/commit",
-  },
-];
+const getActualBread = (isRejected: boolean): Route[] => {
+  return [
+    {
+      name: "Inicio",
+      path: "/",
+    },
+    {
+      name: "Webpay Plus",
+      path: "/webpay-plus",
+    },
+    {
+      name: isRejected ? "" : "Confirmar transacción",
+      path: "/webpay-plus/commit",
+    },
+  ];
+};
+
+const commitedContent = {
+  title: "Webpay Plus - Confirmar transacción",
+  description: (
+    <>
+      En este paso es importante confirmar la transacción para notificar a
+      Transbank que hemos recibido exitosamente los detalles de la transacción.{" "}
+      <b>
+        Es importante destacar que si la confirmación no se realiza, la
+        transacción será reversada.
+      </b>
+    </>
+  ),
+};
+
+const rejectedContent = {
+  title: "Webpay Plus - Rechazo Bancario",
+  description: (
+    <span>
+      En esta fase, pueden surgir inconvenientes, ya sea con el titular de la
+      tarjeta o a nivel bancario, lo que resulta en el estado final de la
+      transacción siendo marcado como &quot;FAILED&quot;.
+    </span>
+  ),
+};
 
 export default async function CommitTransaction({
   searchParams,
 }: NextPageProps) {
   const { token_ws } = searchParams;
-  const { commitResponse } = await commitTransaction(token_ws as string);
+  const { type, commitResponse, abortedResponse, timeoutResponse } =
+    await commitTransaction(searchParams as SearchParams);
+
+  if (type === TBKCallbackType.ABORTED) {
+    return (
+      <AbortedView abortedResponse={abortedResponse as TBKAbortedResponse} />
+    );
+  }
+
+  if (type === TBKCallbackType.TIMEOUT) {
+    return (
+      <TimeoutView
+        token_ws={token_ws as string}
+        timeoutResponse={timeoutResponse as TBKTimeoutResponse}
+      />
+    );
+  }
+
+  const isTransactionRejected =
+    commitResponse?.status === TBKTransactionStatus.FAILED;
+
+  const content = isTransactionRejected ? rejectedContent : commitedContent;
 
   return (
     <>
@@ -34,26 +91,21 @@ export default async function CommitTransaction({
         <title>Transbank SDK Node - Commit Transaction</title>
       </Head>
       <Layout
-        pageTitle="Webpay Plus - Confirmar transacción"
-        pageDescription={
-          <>
-            En este paso es importante confirmar la transacción para notificar a
-            Transbank que hemos recibido exitosamente los detalles de la
-            transacción.{" "}
-            <b>
-              Es importante destacar que si la confirmación no se realiza, la
-              transacción será reversada.
-            </b>
-          </>
-        }
-        actualBread={actualBread}
+        pageTitle={content.title}
+        pageDescription={content.description}
+        actualBread={getActualBread(isTransactionRejected)}
         activeRoute="/webpay-plus/commit"
-        steps={getCommitSteps(token_ws as string, commitResponse)}
+        steps={getCommitSteps(
+          token_ws as string,
+          commitResponse as TBKCommitTransactionResponse
+        )}
         additionalContent={
-          <RefundAndStatus
-            token={token_ws as string}
-            amount={commitResponse.amount}
-          />
+          !isTransactionRejected && (
+            <RefundAndStatus
+              token={token_ws as string}
+              amount={(commitResponse as TBKCommitTransactionResponse).amount}
+            />
+          )
         }
       />
     </>
