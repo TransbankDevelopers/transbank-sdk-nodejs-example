702bc2043d049a01e4eac741fdbbb86125463797
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-14 08:59:07 -0400

    feat: error pages for webpay plus deferred

---
+++ src/app/webpay-plus-deferred/commit/error/aborted.tsx
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import { Route } from "@/types/menu";
+import { Layout } from "@/components/layout/Layout";
+import { LayoutContent } from "@/components/layout/LayoutContent";
+import { getStatusTRXSteps } from "../../content/steps/status";
+import { getStatusTransaction } from "@/app/lib/webpay-plus/data";
+import { getErrorAbortedSteps } from "../../content/steps/error-aborted";
+import { TBKAbortedResponse } from "@/types/transactions";
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Plus Diferido",
+    path: "/webpay-plus-deferred",
+  },
+  {
+    name: "Estado cancelada",
+    path: "/webpay-plus-deferred/commit",
+  },
+];
+
+export type AbortedViewProps = {
+  abortedResponse: TBKAbortedResponse;
+};
+
+export const AbortedView = async (props: AbortedViewProps) => {
+  const statusResponse = await getStatusTransaction(
+    props.abortedResponse.token_ws
+  );
+  return (
+    <>
+      <Head>
+        <title>Transbank SDK Node - Estado cancelada</title>
+      </Head>
+      <Layout
+        pageTitle="Webpay Plus - Estado de compra cancelada"
+        pageDescription={
+          <span>
+            El pago de la compra ha sido anulado por el usuario. En esta etapa,
+            después de abandonar el formulario de pago, no es necesario realizar
+            la confirmación. Aquí te proporcionamos información esencial sobre
+            el estado de la transacción anulada:
+          </span>
+        }
+        actualBread={actualBread}
+        activeRoute="/webpay-plus-deferred/commit"
+        steps={getErrorAbortedSteps(props.abortedResponse)}
+        additionalContent={
+          <div className="aborted-status">
+            <LayoutContent
+              pageTitle="Consulta de Estado de Transacción"
+              pageDescription="Puedes solicitar el estado de una transacción hasta 7 días después de su realización. No hay límite de solicitudes de este tipo durante ese período. Sin embargo, después de pasar los 7 días, ya no podrás revisar el estado de la transacción."
+              steps={getStatusTRXSteps(
+                props.abortedResponse.token_ws,
+                statusResponse
+              )}
+            />
+          </div>
+        }
+      />
+    </>
+  );
+};
---
+++ src/app/webpay-plus-deferred/commit/error/invalid.tsx
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import { Route } from "@/types/menu";
+import { Layout } from "@/components/layout/Layout";
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Plus",
+    path: "/webpay-plus",
+  },
+  {
+    name: "Recuperar transacción",
+    path: "/webpay-plus/commit",
+  },
+];
+
+export type InvalidPaymentViewProps = {
+  //   abortedResponse: TBKAbortedResponse;
+};
+
+export const InvalidPaymentView = async (props: InvalidPaymentViewProps) => {
+  return (
+    <>
+      <Head>
+        <title>Transbank SDK Node - Recuperar transacción</title>
+      </Head>
+      <Layout
+        pageTitle="Webpay Plus - Recuperar transacción"
+        pageDescription={
+          <span>
+            Se ha producido un error en el formulario de pago. Si ha hecho clic
+            en el enlace &quot;volver al sitio&quot; desde la pantalla de error
+            después de cerrar inesperadamente la pestaña del navegador y trata
+            de recuperarla, es posible que haya recibido los siguientes tokens:
+            token_ws, TBK_TOKEN, TBK_ID_SESION, TBK_ORDEN_COMPRA.
+          </span>
+        }
+        actualBread={actualBread}
+        activeRoute="/webpay-plus/commit"
+        steps={[]}
+      />
+    </>
+  );
+};
