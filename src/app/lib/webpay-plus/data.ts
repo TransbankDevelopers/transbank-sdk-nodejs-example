7a2418da713b8b7aee16361295e43991b8046871
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-14 08:50:57 -0400

    refactor: make methods on data.ts dynamic and reusable

--- src/app/lib/webpay-plus/data.ts
+++ src/app/lib/webpay-plus/data.ts
@@ -1,28 +1,55 @@
 import { generateRandomTransactionData } from "@/helpers/webpay-plus/transactionHelper";
+import { SearchParams } from "@/types/general";
 import {
+  CommitTransactionResult,
   StartTransactionData,
+  TBKCallbackType,
   TBKCommitTransactionResponse,
   TBKCreateTransactionResponse,
   TBKTransactionStatusResponse,
 } from "@/types/transactions";
 import { headers } from "next/headers";
-import { WebpayPlus } from "transbank-sdk";
+import { Options, WebpayPlus } from "transbank-sdk";
 
 export type CreateTransactionResult = TBKCreateTransactionResponse &
   StartTransactionData;
 
-export const createTransaction = async (): Promise<CreateTransactionResult> => {
+const getCallbackType = (parameters: SearchParams): TBKCallbackType => {
+  const { token_ws, TBK_TOKEN, TBK_ORDEN_COMPRA, TBK_ID_SESION } = parameters;
+
+  if (token_ws && !(TBK_TOKEN && TBK_ORDEN_COMPRA && TBK_ID_SESION)) {
+    return TBKCallbackType.COMMIT_OK;
+  }
+
+  if (TBK_ID_SESION && TBK_ORDEN_COMPRA && !TBK_TOKEN) {
+    return TBKCallbackType.TIMEOUT;
+  }
+
+  if (TBK_ID_SESION && TBK_ORDEN_COMPRA && TBK_TOKEN) {
+    return TBKCallbackType.ABORTED;
+  }
+
+  return TBKCallbackType.INVALID_PAYMENT;
+};
+
+export const createTransaction = async (
+  returnRoute: string = "/webpay-plus/commit",
+  options?: Options
+): Promise<CreateTransactionResult> => {
   const headersList = headers();
   const protocol = headersList.get("x-forwarded-proto") || "http"; // https://github.com/vercel/next.js/issues/2469
   const host = headersList.get("host") || "localhost:3000";
 
   const startTransactionData = generateRandomTransactionData(
     protocol as string,
-    host
+    host,
+    returnRoute
   );
 
   const createResponse: TBKCreateTransactionResponse =
-    await new WebpayPlus.Transaction(WebpayPlus.getDefaultOptions()).create(
+    await new WebpayPlus.Transaction(
+      options ?? WebpayPlus.getDefaultOptions()
+    ).create(
       startTransactionData.buyOrder,
       startTransactionData.sessionId,
       startTransactionData.amount,
@@ -35,35 +62,75 @@ export const createTransaction = async (): Promise<CreateTransactionResult> => {
   };
 };
 
-export type CommitTransactionResult = {
-  commitResponse: TBKCommitTransactionResponse;
-};
-
 export const commitTransaction = async (
-  token_ws: string
+  parametersReceivedByTBK: SearchParams,
+  options?: Options
 ): Promise<CommitTransactionResult> => {
-  const commitResponse: TBKCommitTransactionResponse =
-    await new WebpayPlus.Transaction(WebpayPlus.getDefaultOptions()).commit(
-      token_ws as string
-    );
+  const callbackType = getCallbackType(parametersReceivedByTBK);
+
+  if (callbackType === TBKCallbackType.COMMIT_OK) {
+    const commitResponse: TBKCommitTransactionResponse =
+      await new WebpayPlus.Transaction(
+        options ?? WebpayPlus.getDefaultOptions()
+      ).commit(parametersReceivedByTBK.token_ws as string);
+
+    return {
+      type: TBKCallbackType.COMMIT_OK,
+      commitResponse,
+    };
+  }
+
+  if (callbackType === TBKCallbackType.ABORTED) {
+    const { token_ws, TBK_TOKEN, TBK_ORDEN_COMPRA, TBK_ID_SESION } =
+      parametersReceivedByTBK;
+
+    return {
+      type: TBKCallbackType.ABORTED,
+      abortedResponse: {
+        token_ws: token_ws as string,
+        TBK_TOKEN: TBK_TOKEN as string,
+        TBK_ORDEN_COMPRA: TBK_ORDEN_COMPRA as string,
+        TBK_ID_SESION: TBK_ID_SESION as string,
+      },
+    };
+  }
+
+  if (callbackType === TBKCallbackType.TIMEOUT) {
+    const { TBK_ORDEN_COMPRA, TBK_ID_SESION } = parametersReceivedByTBK;
+
+    return {
+      type: TBKCallbackType.TIMEOUT,
+      timeoutResponse: {
+        TBK_ORDEN_COMPRA: TBK_ORDEN_COMPRA as string,
+        TBK_ID_SESION: TBK_ID_SESION as string,
+      },
+    };
+  }
 
   return {
-    commitResponse,
+    type: callbackType,
   };
 };
 
-export const getStatusTransaction = async (token_ws: string) => {
+export const getStatusTransaction = async (
+  token_ws: string,
+  options?: Options
+) => {
   const trxStatus: TBKTransactionStatusResponse =
-    await new WebpayPlus.Transaction(WebpayPlus.getDefaultOptions()).status(
-      token_ws as string
-    );
+    await new WebpayPlus.Transaction(
+      options ?? WebpayPlus.getDefaultOptions()
+    ).status(token_ws as string);
 
   return trxStatus;
 };
 
-export const refundTransaction = async (token_ws: string, amount: number) => {
+export const refundTransaction = async (
+  token_ws: string,
+  amount: number,
+  options?: Options
+) => {
   const refundResponse = await new WebpayPlus.Transaction(
-    WebpayPlus.getDefaultOptions()
+    options ?? WebpayPlus.getDefaultOptions()
   ).refund(token_ws as string, amount);
 
   return refundResponse;
