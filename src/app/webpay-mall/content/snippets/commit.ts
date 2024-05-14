fe76fd70ad9ecdc98220dad9ed0e5bae015603d8
victor mendoza <victor.mendoza@continuumhq.com>
2024-05-14 18:47:33 -0300

    feat: prepare snippet for mall transactions

--- src/app/webpay-mall/content/snippets/commit.ts
+++ src/app/webpay-mall/content/snippets/commit.ts
@@ -1,4 +1,4 @@
-import { TBKCommitTransactionResponse } from "@/types/transactions";
+import { TBKMallCommitTransactionResponse } from "@/types/transactions";
 
 export const getStepOne = (token: string) => {
   return `{
@@ -9,26 +9,24 @@ export const getStepOne = (token: string) => {
 export const getStepTwo = () => {
   return `// En el caso de Express
 let token = request.body.token_ws;
-const commitResponse = await (new WebpayPlus.Transaction()).commit(token);`;
+const commitResponse = await (new WebpayPlus.MallTransaction()).commit(token);`;
 };
 
-export const getStepThree = (commitResponse: TBKCommitTransactionResponse) => {
+export const getStepThree = (
+  commitResponse: TBKMallCommitTransactionResponse
+) => {
   const {
-    amount,
-    status,
     buy_order,
     session_id,
     card_detail,
     accounting_date,
     transaction_date,
-    authorization_code,
-    payment_type_code,
-    response_code,
-    installments_number,
+    vci,
+    details,
   } = commitResponse;
+
   return `{
-  "amount": ${amount},
-  "status": "${status}",
+  "vci": "${vci}",
   "buy_order": "${buy_order}",
   "session_id": "${session_id}",
   "card_detail": {
@@ -36,9 +34,29 @@ export const getStepThree = (commitResponse: TBKCommitTransactionResponse) => {
   },
   "accounting_date": "${accounting_date}",
   "transaction_date": "${transaction_date}",
-  "authorization_code": "${authorization_code}",
-  "payment_type_code": "${payment_type_code}",
-  "response_code": "${response_code}",
-  "installments_number": "${installments_number}"
-  }`;
+  "details": [
+    {
+      "amount": ${details[0].amount},
+      "status": "${details[0].status}",
+      "authorization_code": "${details[0].authorization_code}",
+      "payment_type_code": "${details[0].payment_type_code}",
+      "response_code": ${details[0].response_code},
+      "installments_number": ${details[0].installments_number},
+      "commerce_code": "${details[0].commerce_code}",
+      "buy_order": "${details[0].buy_order}"
+    },
+    {
+      "amount": ${details[1].amount},
+      "status": "${details[1].status}",
+      "authorization_code": "${details[1].authorization_code}",
+      "payment_type_code": "${details[1].payment_type_code}",
+      "response_code": ${details[1].response_code},
+      "installments_number": ${details[1].installments_number},
+      "commerce_code": "${details[1].commerce_code}",
+      "buy_order": "${details[1].buy_order}"
+    }
+  ],
+  }
+  
+ `;
 };
