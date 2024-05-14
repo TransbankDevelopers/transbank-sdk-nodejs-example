9af2cf4c2afb5ebe6ebc544884442d5471725365
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-14 08:59:26 -0400

    refactor: improve step component props handling

--- src/components/step/Step.tsx
+++ src/components/step/Step.tsx
@@ -3,15 +3,17 @@ import { Text, TextType } from "../text/Text";
 
 export type StepProps = {
   code?: string;
-  stepTitle: string;
+  stepTitle?: string;
   stepId?: string;
-  content: string | React.ReactNode;
+  content?: string | React.ReactNode;
 };
 
 export const Step = (props: StepProps) => {
   return (
     <div className="flex-col" id={props.stepId}>
-      <Text type={TextType.SECTION_TITLE}>{props.stepTitle}</Text>
+      {props.stepTitle && (
+        <Text type={TextType.SECTION_TITLE}>{props.stepTitle}</Text>
+      )}
       {props.content}
       {props.code && (
         <div className="mt-6">
