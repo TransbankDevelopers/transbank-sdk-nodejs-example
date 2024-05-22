b89dac51a69a50057e9380e51c445597c81a75f3
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-22 10:35:00 -0400

    feat: remove user page for oneclick mall deferred

---
+++ src/app/oneclick-mall-deferred/remove-user/page.tsx
@@ -0,0 +1,52 @@
+import { Route } from "@/types/menu";
+import { NavigationItem } from "@/components/layout/Navigation";
+import { removeUserInscriptionOneclick } from "@/app/lib/oneclick-mall/data";
+import { NextPageProps } from "@/types/general";
+import { Layout } from "@/components/layout/Layout";
+import Head from "next/head";
+import { getRemoveUserSteps } from "../content/steps/remove-user";
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Oneclick",
+    path: "/oneclick-mall",
+  },
+  {
+    name: "Borrar usuario",
+    path: "/oneclick-mall/remove-user",
+  },
+];
+
+const navigationItems: NavigationItem[] = [
+  {
+    title: "Ejemplo",
+    sectionId: "peticion",
+  },
+];
+
+export default async function RemoveUserInscriptionOneClick({
+  searchParams,
+}: NextPageProps) {
+  const { tbk_user, user_name } = searchParams;
+  await removeUserInscriptionOneclick(tbk_user as string, user_name as string);
+
+  return (
+    <>
+      <Head>
+        <title>Transbank SDK Node - Finalizar inscripción</title>
+      </Head>
+      <Layout
+        pageTitle="Webpay Oneclick Mall - Finalizar inscripción"
+        pageDescription="En esta fase, completaremos el proceso de inscripción, permitiéndonos posteriormente realizar cargos a la tarjeta que el tarjetahabiente haya inscrito."
+        actualBread={actualBread}
+        activeRoute="/oneclick-mall/remove-user"
+        navigationItems={navigationItems}
+        steps={getRemoveUserSteps(tbk_user as string, user_name as string)}
+      />
+    </>
+  );
+}
