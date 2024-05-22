0870a659df22e99db3e420e5a52ad6d97dd58010
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-22 00:04:37 -0400

    feat: add status page for oneclick mall

---
+++ src/app/oneclick-mall/status/page.tsx
@@ -0,0 +1,55 @@
+import { Route } from "@/types/menu";
+import { Layout } from "@/components/layout/Layout";
+import Head from "next/head";
+import { getStatusTRXSteps } from "@/app/oneclick-mall/content/steps/status";
+import { NextPageProps } from "@/types/general";
+import { NavigationItem } from "@/components/layout/Navigation";
+import { getStatusOneclickMallTransaction } from "@/app/lib/oneclick-mall/data";
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Oneclick",
+    path: "/oneclick-mall",
+  },
+  {
+    name: "Estado de transacción",
+    path: "/oneclick-mall/status",
+  },
+];
+
+const navigationItems: NavigationItem[] = [
+  {
+    title: "Petición",
+    sectionId: "peticion",
+  },
+  {
+    title: "Respuesta",
+    sectionId: "respuesta",
+  },
+];
+
+export default async function StatusTransactionView({
+  searchParams,
+}: NextPageProps) {
+  const { buy_order } = searchParams;
+  const trxStatus = await getStatusOneclickMallTransaction(buy_order as string);
+  return (
+    <>
+      <Head>
+        <title>Transbank SDK Node - Estado de transacción</title>
+      </Head>
+      <Layout
+        pageTitle="Webpay Oneclick Mall - Estado de transacción"
+        pageDescription={`Con esta operación, puedes solicitar el estado actual de una transacción en cualquier momento.`}
+        actualBread={actualBread}
+        activeRoute="/oneclick-mall/status"
+        steps={getStatusTRXSteps(buy_order as string, trxStatus)}
+        navigationItems={navigationItems}
+      />
+    </>
+  );
+}
