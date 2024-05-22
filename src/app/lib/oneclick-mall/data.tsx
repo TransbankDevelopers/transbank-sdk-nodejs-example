d6879d5d7824e42ad02f50f6fa75276872e72656
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-22 10:31:35 -0400

    refactor: make data layer for oneclick mall reusable for deferred

--- src/app/lib/oneclick-mall/data.tsx
+++ src/app/lib/oneclick-mall/data.tsx
@@ -34,28 +34,43 @@ export const getOneclickMallOptions = () => {
   );
 };
 
-export const createOneclickMallTransaction =
-  async (): Promise<CreateTransactionResult> => {
-    const headersList = headers();
-    const protocol = headersList.get("x-forwarded-proto") || "http"; // https://github.com/vercel/next.js/issues/2469
-    const host = headersList.get("host") || "localhost:3000";
+// TODO: check if this function needs to be moved to a different file in the future
+export const getOneclickMallDeferredOptions = () => {
+  return new Options(
+    IntegrationCommerceCodes.ONECLICK_MALL_DEFERRED,
+    IntegrationApiKeys.WEBPAY,
+    Environment.Integration
+  );
+};
 
-    const trxData = generateRandomTransactionDataOneclickMall(protocol, host);
+export const createOneclickMallTransaction = async (
+  isDeferred?: boolean
+): Promise<CreateTransactionResult> => {
+  const headersList = headers();
+  const protocol = headersList.get("x-forwarded-proto") || "http"; // https://github.com/vercel/next.js/issues/2469
+  const host = headersList.get("host") || "localhost:3000";
 
-    const { userName, email, returnUrl } = trxData;
+  const trxData = generateRandomTransactionDataOneclickMall(
+    protocol,
+    host,
+    isDeferred || false
+  );
 
-    const startResponse = await new Oneclick.MallInscription(
-      getOneclickMallOptions()
-    ).start(userName, email, returnUrl);
+  const { userName, email, returnUrl } = trxData;
 
-    return { ...startResponse, ...trxData };
-  };
+  const startResponse = await new Oneclick.MallInscription(
+    isDeferred ? getOneclickMallDeferredOptions() : getOneclickMallOptions()
+  ).start(userName, email, returnUrl);
+
+  return { ...startResponse, ...trxData };
+};
 
 export const finishOneclickMallTransaction = async (
-  token: string
+  token: string,
+  isDeferred?: boolean
 ): Promise<TBKFinishInscriptionResponse> => {
   const commitResponse = await new Oneclick.MallInscription(
-    getOneclickMallOptions()
+    isDeferred ? getOneclickMallDeferredOptions() : getOneclickMallOptions()
   ).finish(token);
 
   return commitResponse;
@@ -73,26 +88,38 @@ export const removeUserInscriptionOneclick = async (
 
 export const authorizeOneClickMallTransaction = async (
   userName: string,
-  tbkUser: string
+  tbkUser: string,
+  deferredAmount?: number,
+  deferredInstallments?: number
 ): Promise<TBKAuthorizeTransactionResponse> => {
   const details: TransactionDetail[] = [];
   const commerceCodes = [
-    IntegrationCommerceCodes.ONECLICK_MALL_CHILD1,
-    IntegrationCommerceCodes.ONECLICK_MALL_CHILD2,
+    deferredAmount
+      ? IntegrationCommerceCodes.ONECLICK_MALL_CHILD1_DEFERRED
+      : IntegrationCommerceCodes.ONECLICK_MALL_CHILD1,
+    deferredAmount
+      ? IntegrationCommerceCodes.ONECLICK_MALL_CHILD2_DEFERRED
+      : IntegrationCommerceCodes.ONECLICK_MALL_CHILD2,
   ];
+
   const buyOrder = "O-" + Math.floor(Math.random() * 10000) + 1;
 
   for (const childCommerceCode of commerceCodes) {
     const childBuyOrder = "O-" + Math.floor(Math.random() * 10000) + 1;
-    const amount = Math.floor(Math.random() * 1000) + 1001;
+    const amount = deferredAmount ?? Math.floor(Math.random() * 1000) + 1001;
 
     details.push(
-      new TransactionDetail(amount, childCommerceCode, childBuyOrder)
+      new TransactionDetail(
+        amount,
+        childCommerceCode,
+        childBuyOrder,
+        deferredInstallments ?? undefined
+      )
     );
   }
 
   const authorizeResponse = await new Oneclick.MallTransaction(
-    getOneclickMallOptions()
+    deferredAmount ? getOneclickMallDeferredOptions() : getOneclickMallOptions()
   ).authorize(userName, tbkUser, buyOrder, details);
 
   return authorizeResponse;
