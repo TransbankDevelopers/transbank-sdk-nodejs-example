539a143911cad174a7ec4387b4ea514046f57207
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-27 12:11:31 -0400

    feat: initial page for full transaction mall

---
+++ src/app/full-transaction-mall/page.tsx
@@ -0,0 +1,27 @@
+import { Route } from "@/types/menu";
+import { NavigationItem } from "@/components/layout/Navigation";
+import { PageContent } from "./content/PageContent";
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Transaccion Completa",
+    path: "/fulltransaction-mall",
+  },
+];
+
+const navigationItems: NavigationItem[] = [
+  {
+    title: "Formulario",
+    sectionId: "formulario",
+  },
+];
+
+export default async function CreateFullTransactionMallTransaction() {
+  return (
+    <PageContent actualBread={actualBread} navigationItems={navigationItems} />
+  );
+}
