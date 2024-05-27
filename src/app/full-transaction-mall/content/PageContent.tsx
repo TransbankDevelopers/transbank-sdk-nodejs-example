5d3bff86825768aae5abf7fcb8056c8b9d17bbf3
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-27 12:13:42 -0400

    feat: page content for initial page on full transaction mall

---
+++ src/app/full-transaction-mall/content/PageContent.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { Layout } from "@/components/layout/Layout";
+import Head from "next/head";
+import { Route } from "@/types/menu";
+import { NavigationItem } from "@/components/layout/Navigation";
+import { CreditCard } from "@/components/creditcard/CreditCard";
+import { useState } from "react";
+import { Focused } from "react-credit-cards-2";
+
+export type PageContentProps = {
+  actualBread: Route[];
+  navigationItems: NavigationItem[];
+};
+
+export const PageContent = (props: PageContentProps) => {
+  const [cardState, setCardState] = useState<CreditCard>({
+    number: "",
+    expiry: "",
+    cvc: "",
+    name: "Ariel Cardenas",
+    focus: "number",
+  });
+
+  const handleInputChange = (value: string, name: string) => {
+    setCardState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleInputFocus = (name: string) => {
+    setCardState((prev) => ({
+      ...prev,
+      focus: name as Focused,
+    }));
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Transbank SDK Node - Formulario</title>
+      </Head>
+      <Layout
+        pageTitle="Transacción Completa Mall - Formulario"
+        pageDescription="En esta primera etapa necesitas obtener los datos esenciales de la tarjeta de crédito del titular. Utiliza el formulario para recolectar esta información de manera segura."
+        actualBread={props.actualBread}
+        activeRoute="/fulltransaction-mall"
+        navigationItems={props.navigationItems}
+        additionalContent={
+          <CreditCard
+            {...cardState}
+            onPay={() => {
+              //
+            }}
+            handleInputChange={handleInputChange}
+            handleInputFocus={handleInputFocus}
+          />
+        }
+      />
+    </>
+  );
+};
