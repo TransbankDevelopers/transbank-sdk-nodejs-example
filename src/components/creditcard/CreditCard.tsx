cad0ae62478788e0af161270327be67838174f2e
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-30 16:24:00 -0400

    refactor: update credit card component to include commit link

--- src/components/creditcard/CreditCard.tsx
+++ src/components/creditcard/CreditCard.tsx
@@ -1,25 +1,30 @@
 import { Card } from "../card/Card";
 import Cards, { Focused } from "react-credit-cards-2";
 import { InputText } from "../input/InputText";
-import { Button } from "../button/Button";
+import { Button, ButtonTypes } from "../button/Button";
 import "react-credit-cards-2/dist/es/styles-compiled.css";
 import "./CreditCard.css";
 import Image from "next/image";
 import Credit from "@/assets/svg/credit.svg";
+import { Url } from "next/dist/shared/lib/router/router";
 
 export type CreditCard = {
   number: string;
   expiry: string;
   cvc: string;
   name: string;
-  focus: Focused;
 };
 
+export type CreditCardState = {
+  focus: Focused;
+} & CreditCard;
+
 export type CreditCardProps = {
-  onPay: () => void;
+  onPay?: () => void;
   handleInputChange: (value: string, name: string) => void;
   handleInputFocus: (name: string) => void;
-} & CreditCard;
+  commitLink?: Url;
+} & CreditCardState;
 
 export const CreditCard = (props: CreditCardProps) => {
   return (
@@ -71,7 +76,7 @@ export const CreditCard = (props: CreditCardProps) => {
           <div className="card-companies">
             <Image src={Credit} alt="credit" />
           </div>
-          <Button text="PAGAR" onClick={props.onPay} />
+          <Button text="PAGAR" link={props.commitLink} onClick={props.onPay} />
         </div>
       </div>
     </Card>
