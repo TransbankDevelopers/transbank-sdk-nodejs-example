8153dca361dbf96e862feb0dd4e3e5967c877f6f
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-05-30 16:18:11 -0400

    feat: add route to create full transaction mall transaction

---
+++ src/app/api/full-transaction-mall/create/route.ts
@@ -0,0 +1,56 @@
+import { createFullTransactionMallTransaction } from "@/app/lib/full-transaction-mall/data";
+import { CreditCard } from "@/components/creditcard/CreditCard";
+import { localStorageFullTransactionDetails } from "@/consts";
+import { NextApiRequest } from "next";
+import { cookies } from "next/headers";
+
+export async function POST(req: NextApiRequest) {
+  const cookiesStore = cookies();
+  const chunks = [];
+
+  for await (const chunk of req.body) {
+    chunks.push(chunk);
+  }
+
+  const body = JSON.parse(Buffer.concat(chunks).toString());
+
+  if (!body) {
+    console.log("No credit card data found");
+    return;
+  }
+
+  const requiredFields = ["number", "expiry", "cvc", "name"];
+
+  for (const field of requiredFields) {
+    if (!body[field]) {
+      console.log(`Missing required field: ${field}`);
+    }
+  }
+
+  const cardState: Omit<CreditCard, "focus"> = {
+    number: body.number,
+    expiry: body.expiry,
+    cvc: body.cvc,
+    name: body.name,
+  };
+
+  try {
+    const { token, details } = await createFullTransactionMallTransaction(
+      cardState
+    );
+
+    cookiesStore.set(
+      localStorageFullTransactionDetails,
+      JSON.stringify(details)
+    );
+
+    return new Response(token, {
+      status: 200,
+    });
+  } catch (_error) {
+    const error = _error as Error;
+    return new Response(error.message, {
+      status: 500,
+    });
+  }
+}
