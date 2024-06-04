d03b429ef4f3d7e0609b35ca57023a23c7b33f6e
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2024-06-04 13:04:59 -0400

    feat: add installments page for full transaction mall deferred

---
+++ src/app/transaccion-completa-mall-diferido/installments/page.tsx
@@ -0,0 +1,83 @@
+"use server";
+import { Layout } from "@/components/layout/Layout";
+import Head from "next/head";
+import { NavigationItem } from "@/components/layout/Navigation";
+import { Route } from "@/types/menu";
+import { NextPageProps } from "@/types/general";
+import { cookies } from "next/headers";
+import { setupInstallmentsFullTransactionMall } from "@/app/lib/transaccion-completa-mall/data";
+import { localStorageFullTransactionDetails } from "@/consts";
+import { TransactionDetail } from "transbank-sdk";
+import { getInstallmentsSteps } from "../content/steps/installments";
+import { ConfirmCard } from "@/app/transaccion-completa-mall/installments/components/ConfirmCard";
+
+const navigationItems: NavigationItem[] = [
+  {
+    title: "Petición",
+    sectionId: "peticion",
+  },
+  { title: "Respuesta", sectionId: "respuesta" },
+  { title: "Confirmar", sectionId: "listo" },
+];
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Transacción Completa Mall Diferido",
+    path: "/transaccion-completa-mall-diferido",
+  },
+  {
+    name: "Consulta de cuotas",
+    path: "/transaccion-completa-mall-diferido/installments",
+  },
+];
+
+export default async function InstallmentsFullTransactionMallPage({
+  searchParams,
+}: NextPageProps) {
+  const { token_ws, installments_number } = searchParams;
+  const cookiesStore = cookies();
+  const data = cookiesStore.get(localStorageFullTransactionDetails);
+
+  if (!data) {
+    console.log("No data found");
+    return;
+  }
+
+  const trxData = await setupInstallmentsFullTransactionMall(
+    token_ws,
+    JSON.parse(data.value) as TransactionDetail[],
+    Number(installments_number),
+    true
+  );
+
+  return (
+    <>
+      <Head>
+        <title>Transbank SDK Node - Confirmar Transacción</title>
+      </Head>
+      <Layout
+        pageTitle="Transacción Completa Mall Diferido - Consulta de cuotas"
+        pageDescription="En esta etapa, realizaremos una consulta de cuotas para conocer sus condiciones. Es importante destacar que este paso es opcional y se utiliza únicamente si deseas ofrecer opciones de pago a plazos."
+        actualBread={actualBread}
+        activeRoute="/transaccion-completa-mall-diferido/installments"
+        navigationItems={navigationItems}
+        steps={getInstallmentsSteps(
+          Array.isArray(trxData) ? trxData[0] : trxData
+        )}
+        additionalContent={
+          <div className="mt-4">
+            <ConfirmCard
+              token={token_ws}
+              idQueryInstallments={String(trxData[0].id_query_installments)}
+              isDeferred
+            />
+          </div>
+        }
+      />
+    </>
+  );
+}
