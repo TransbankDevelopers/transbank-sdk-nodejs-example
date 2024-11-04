8b0cd78dd11445cc46e55fecf33126d59bb9aa64
victor mendoza <victor.mendoza@continuumhq.com>
2024-11-04 15:47:47 -0300

    feat: add meddleware for control flow 4

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+// middleware.ts
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const routeMapping: { [key: string]: string } = {
+    "/webpay-plus/commit": "/api/webpay-plus/commit",
+    "/webpay-mall/commit": "/api/webpay-mall/commit",
+    "/webpay-mall-diferido/commit": "/api/webpay-mall-diferido/commit",
+    "/webpay-plus-deferred/commit": "/api/webpay-plus-deferred/commit",
+    "/oneclick-mall/finish": "/api/oneclick-mall/finish",
+    "/oneclick-mall-deferred/finish": "/api/oneclick-mall-deferred/finish",
+  };
+
+  console.log({ pathname });
+  console.log(pathname in routeMapping);
+  if (request.method === "POST" && pathname in routeMapping) {
+    const apiRoute = routeMapping[pathname];
+    return NextResponse.rewrite(new URL(apiRoute, request.nextUrl.origin));
+  }
+  request.nextUrl.pathname;
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/webpay-plus/commit",
+    "/webpay-mall/commit",
+    "/webpay-mall-diferido/commit",
+    "/webpay-plus-deferred/commit",
+    "/oneclick-mall/finish",
+    "/oneclick-mall-deferred/finish",
+  ],
+};
