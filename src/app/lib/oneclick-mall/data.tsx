94cc723764f4fc13976ec9e0fc9e7e146d2cf06c
victor mendoza <victor.mendoza@continuumhq.com>
2024-11-24 23:36:29 -0300

    feat: homolagate authorize function

--- src/app/lib/oneclick-mall/data.tsx
+++ src/app/lib/oneclick-mall/data.tsx
@@ -28,6 +28,13 @@ type RefundOneClickMallTransactionProps = {
   isDeferred?: boolean;
 };
 
+export interface AuthorizeTransactionOptions {
+  prmDetails?: TransactionDetail[];
+  prmBuyOrder?: string;
+  deferredAmount?: number;
+  deferredInstallments?: number;
+}
+
 export const getOneclickMallOptions = () => {
   return new Options(
     IntegrationCommerceCodes.ONECLICK_MALL,
@@ -82,33 +89,41 @@ export const removeUserInscriptionOneclick = async (
 export const authorizeOneClickMallTransaction = async (
   userName: string,
   tbkUser: string,
-  deferredAmount?: number,
-  deferredInstallments?: number
+  isDeferred?: boolean,
+  options?: AuthorizeTransactionOptions
 ): Promise<TBKAuthorizeTransactionResponse> => {
+  const { prmDetails, prmBuyOrder, deferredAmount, deferredInstallments } =
+    options || {};
   const details: TransactionDetail[] = [];
   const commerceCodes = [
-    deferredAmount
+    isDeferred
       ? IntegrationCommerceCodes.ONECLICK_MALL_CHILD1_DEFERRED
       : IntegrationCommerceCodes.ONECLICK_MALL_CHILD1,
-    deferredAmount
+    isDeferred
       ? IntegrationCommerceCodes.ONECLICK_MALL_CHILD2_DEFERRED
       : IntegrationCommerceCodes.ONECLICK_MALL_CHILD2,
   ];
 
-  const buyOrder = "O-" + Math.floor(Math.random() * 10000) + 1;
+  const buyOrder = prmBuyOrder
+    ? prmBuyOrder
+    : "O-" + Math.floor(Math.random() * 10000) + 1;
 
-  for (const childCommerceCode of commerceCodes) {
-    const childBuyOrder = "O-" + Math.floor(Math.random() * 10000) + 1;
-    const amount = deferredAmount ?? Math.floor(Math.random() * 1000) + 1001;
+  if (prmDetails) {
+    details.push(...prmDetails);
+  } else {
+    for (const childCommerceCode of commerceCodes) {
+      const childBuyOrder = "O-" + Math.floor(Math.random() * 10000) + 1;
+      const amount = deferredAmount ?? Math.floor(Math.random() * 1000) + 1001;
 
-    details.push(
-      new TransactionDetail(
-        amount,
-        childCommerceCode,
-        childBuyOrder,
-        deferredInstallments ?? undefined
-      )
-    );
+      details.push(
+        new TransactionDetail(
+          amount,
+          childCommerceCode,
+          childBuyOrder,
+          deferredInstallments ?? undefined
+        )
+      );
+    }
   }
 
   const authorizeResponse = await new Oneclick.MallTransaction(
