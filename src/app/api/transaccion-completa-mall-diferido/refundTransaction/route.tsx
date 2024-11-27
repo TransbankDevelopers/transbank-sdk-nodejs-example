3f0bae3d35b6f01e70b2887a8aab804d9a5a7c28
victor mendoza <victor.mendoza@continuumhq.com>
2024-11-27 10:43:42 -0300

    feat: add trasaccion completa mall diferido in apiref views

---
+++ src/app/api/transaccion-completa-mall-diferido/refundTransaction/route.tsx
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { refundFullTransactionMallTransaction } from "@/app/lib/transaccion-completa-mall-diferido/data";
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const token = formData.get("token") as string;
+  const commerceCode = formData.get("commerceCode") as string;
+  const buyOrder = formData.get("buyOrder") as string;
+  const amount = Number(formData.get("amount"));
+
+  try {
+    const trxStatus = await refundFullTransactionMallTransaction({
+      token,
+      buyOrder,
+      commerceCode,
+      amount,
+    });
+
+    return NextResponse.json(trxStatus);
+  } catch (error) {
+    console.error(error);
+    let errorMessage = "Ocurrió un error al hacer refund de la transacción.";
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else if (typeof error === "string") {
+      errorMessage = error;
+    }
+
+    return NextResponse.json({ error: errorMessage });
+  }
+}
