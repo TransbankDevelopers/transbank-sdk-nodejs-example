9837a67141b6c735f12c34b8acc8373aba8ebccd
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-11-28 15:11:21 -0300

    Update src/app/api-reference/webpay-mall-deferred/page.tsx
    Co-authored-by: Mauricio Astudillo <m.astudillo.toledo@gmail.com>

--- src/app/api-reference/webpay-mall-deferred/page.tsx
+++ src/app/api-reference/webpay-mall-deferred/page.tsx
@@ -73,7 +73,7 @@ export default function ApiRefWebpayMall() {
             target="_blank"
             className="tbk-link"
           >
-            documentacion
+            documentación
           </a>
         </p>
         <ApiRefWebpayMallDeferredRefund />
