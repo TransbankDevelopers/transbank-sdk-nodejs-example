6fc3b6867144424a2d7b8f4396da3a42e2c5f0ce
DiegoHBustamante <99495937+DiegoHBustamante@users.noreply.github.com>
2024-12-10 17:03:16 -0300

    feat: add breadcrumb navigation for Webpay Oneclick Mall Deferred

--- src/app/oneclick-mall-deferred/page.tsx
+++ src/app/oneclick-mall-deferred/page.tsx
@@ -1,5 +1,18 @@
 import { createOneclickMallTransaction } from "../lib/oneclick-mall/data";
 import { PageContent } from "./content/PageContent";
+import { CustomError } from "@/components/customError/CustomError";
+import { Route } from "@/types/menu";
+
+const actualBread: Route[] = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Webpay Oneclick",
+    path: "/oneclick-mall-deferred",
+  },
+];
 
 export default async function CreateOneclickMallDeferredTransactionView() {
   const trxData = await createOneclickMallTransaction(true);
