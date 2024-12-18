e1c9fdba66857a8e17884874ff34f4fdabe1e6a8
DiegoHBustamante <99495937+DiegoHBustamante@users.noreply.github.com>
2024-12-18 11:46:38 -0300

    feat: add message in CardPay.tsx

--- src/components/cardpay/CardPay.tsx
+++ src/components/cardpay/CardPay.tsx
@@ -1,6 +1,9 @@
 import { Button, ButtonTypes } from "../button/Button";
 import { Card } from "../card/Card";
 import { InputText } from "../input/InputText";
+import { Text } from "@/components/text/Text";
+import InfoBlue from "@/assets/svg/InfoBlue.svg";
+import Image from "next/image";
 
 export enum TokenPropNames {
   token_ws = "token_ws",
@@ -22,16 +25,26 @@ export const CardPay = (props: CardPayProps) => {
     <Card className="card-pay">
       <span className="title">Formulario de redirección</span>
       <InputText label="Token" value={props.token} />
-      <div className="button-container">
-        <form action={props.webpayUrl} method="POST">
-          <input type="hidden" name={tokenInputName} value={props.token} />
+      <form action={props.webpayUrl} method="POST">
+        <input type="hidden" name={tokenInputName} value={props.token} />
+        <div className="tbk-info-token">
+          <Image
+            src={InfoBlue}
+            alt="tbk info blue"
+            className="tbk-info-token-icon"
+          />
+          <Text className="tbk-info-token-text">
+            El token generado en esta aplicación se renueva automáticamente cada 5 minutos.
+          </Text>
+        </div>
+        <div className="button-container">
           <Button
             text={buttonText}
             className="button"
             type={ButtonTypes.SUBMIT}
           />
-        </form>
-      </div>
+        </div>
+      </form>
     </Card>
   );
 };
