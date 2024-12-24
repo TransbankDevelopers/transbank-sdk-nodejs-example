5984a9e6a4491fa01506388f4bbfa9a9aaaf3be7
victor mendoza <victor.mendoza@continuumhq.com>
2024-12-24 10:30:35 -0300

    feat: prevent browser cache

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,27 @@ const nextConfig = {
       ],
     },
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
