3d94a4805804b7e552eab1d796b13bf6cba35928
mvarlic <mvarlic@gmail.com>
2025-02-19 14:34:32 -0500

    feat: add authorize message component

---
+++ src/components/messages/OkAuthorizeMessage.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Text } from "@/components/text/Text";
+
+export const OkAuthorizeMessage = () => {
+  return (
+    <Text>
+      Una vez que la transacción ha sido autorizada Transbank proporcionará la siguiente información. 
+      Es fundamental conservar esta respuesta y verificar que el campo &quot;response_code&quot; 
+      tenga un valor de cero y que el campo &quot;status&quot; sea &quot;AUTHORIZED&quot;.
+    </Text>
+  );
+};
