68e2380a2493d6466869e5ea696d86e14628d3e6
mvarlic <mvarlic@gmail.com>
2025-02-24 12:52:39 -0500

    feat: add message components

---
+++ src/components/messages/OtherOperationsCommitDeferredMessage.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+export const OtherOperationsCommitDeferredMessage = () => {
+  return (
+    <div className="step-ready">
+      <div className="instructions">
+        <p>
+          Ahora que hemos confirmado la transacción, se abren dos opciones para continuar:
+        </p>
+        <ul className="list">
+          <li>
+            <span className="font-bold">Capturar:</span> La transacción necesita ser 
+            capturada para ser autorizada.
+          </li>
+          <li>
+            <span className="font-bold">Consultar Estado:</span> Hasta 7
+            días después de realizada la transacción, podrás consultar el
+            estado de la transacción.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/messages/OtherOperationsCreateFullTxMessage.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export const OtherOperationsCreateFullTxMessage = () => {
+  return (
+    <div className="step-ready">
+      <div className="instructions">
+        <p>
+          Ahora que hemos creado la transacción, se abren dos opciones para continuar:
+        </p>
+        <ul className="list">
+          <li>
+            <span className="font-bold">Consultar Cuotas(opcional):</span> Alternativamente 
+            puedes realizar consultas de cuotas para ofrecer opciones de pago a plazos.
+          </li>
+          <li>
+            <span className="font-bold">Confirmar Transacción:</span> Debes confirmar directamente 
+            la transacción para finalizar con el proceso de pago.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
