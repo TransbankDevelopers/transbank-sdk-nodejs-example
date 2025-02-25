32ac4c6ef405d10bb967b51a7866e78e25cf447e
mvarlic <mvarlic@gmail.com>
2025-02-25 11:02:36 -0500

    feat: update capture message

--- src/components/messages/OperationsCommitDeferredMessage.tsx
+++ src/components/messages/OperationsCommitDeferredMessage.tsx
@@ -4,12 +4,12 @@ export const OperationsCommitDeferredMessage = () => {
     <div className="step-ready">
       <div className="instructions">
         <p>
-          Ahora que hemos confirmado la transacción, se abren dos opciones para continuar:
+          Ahora que se ha confirmado la transacción, puedes capturar el monto previamente autorizado. 
+          El monto a capturar puede ser igual o menor al monto autorizado. La captura debe efectuarse dentro de un plazo máximo de 7 días calendario.
         </p>
         <ul className="list">
           <li>
-            <span className="font-bold">Capturar:</span> La transacción necesita ser 
-            capturada para ser autorizada.
+            <span className="font-bold">Capturar:</span> Captura un monto igual o menor al previamente autorizado.
           </li>
           <li>
             <span className="font-bold">Consultar Estado:</span> Hasta 7
