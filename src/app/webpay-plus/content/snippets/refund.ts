4f0d2cb3c2489eeb3dd069fec1c38ac180798904
Miguel Villalobos <miguel.villalobos@continuumhq.com>
2025-05-07 17:14:30 -0400

    docs: snippets updated for Webpay Plus

--- src/app/webpay-plus/content/snippets/refund.ts
+++ src/app/webpay-plus/content/snippets/refund.ts
@@ -3,7 +3,12 @@ import { TBKRefundTransactionResponse } from "@/types/transactions";
 export const getStepOne = (amount: string) => {
   return `// Token: 01ab8fb16e5dee67fcc392b97d679a01d29b77b4cd8b9ee6ade278203feee1b4
 // Amount: ${amount}
-const refundRequest = await (new WebpayPlus.Transaction()).refund(token, amount);`;
+const tx = new WebpayPlus.Transaction(new Options(
+  IntegrationCommerceCodes.WEBPAY_PLUS,
+  IntegrationApiKeys.WEBPAY,
+  Environment.Integration
+));
+const refundRequest = await tx.refund(token, amount);`;
 };
 
 export const getStepTwo = (refundResult: TBKRefundTransactionResponse) => {
